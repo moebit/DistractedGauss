7a2d7f4df5cfe85ab82dc8e58bc2740bbf2ca719
M0e <mghas2@uic.edu>
2017-10-24 18:02:07 -0500

    :tada: add initial pop-up js

---
+++ js/attentionCenter.js
@@ -0,0 +1,42 @@
+"use strict";
+
+window.addEventListener('load', () => {
+    //get setting info from local storage when page loads
+    chrome.storage.local.get(['isRunning', 'removeSug', 'theaterMode', 'removeComments'], value => {
+        document.getElementById('control-button').innerText = (value["isRunning"] ? "Pause" : "Resume");
+        value['removeSug'] != null ? document.getElementById('remove-suggestions').checked = value["removeSug"] : null;
+        value['theaterMode'] != null ? document.getElementById('theatermode').checked = value["theaterMode"] : null;
+        value['removeComments'] != null ? document.getElementById('remove-comments').checked = value["removeComments"] : null;
+    });
+    //get element info of loaded page
+    document.getElementById('control-button').addEventListener('click', () => {chrome.storage.local.get(["isRunning"], value => {controlCommand("control", value["isRunning"]);}) });
+    document.getElementById('save-settings-button').addEventListener('click', () => (saveSettings(), document.getElementById('save-settings-button').disabled = true));
+    //any changes in checkboxes  
+    Array.prototype.map.call(document.getElementsByClassName('settings'), checkbox => checkbox.addEventListener('change', () => document.getElementById('save-settings-button').disabled = false)); 
+
+});
+
+var saveSettings = () => {
+    chrome.storage.local.set({
+        "removeSug": document.getElementById('remove-suggestions').checked,
+        "theaterMode": document.getElementById('theatermode').checked,
+        "removeComments": document.getElementById('remove-comments').checked
+    });
+    controlCommand("save_settings");
+};
+
+var controlCommand = (type, value) => {
+    switch (type) {
+        case "control":
+            value ? chrome.runtime.sendMessage({command: "pause"}) : chrome.runtime.sendMessage({command: "resume"});
+            document.getElementById('control-button').innerText = (value ? "Resume" : "Pause");
+            break;
+        case "save_settings":
+            chrome.runtime.sendMessage({command: "refreshSettings"})
+            break;
+    }
+};
+
+
+
+
