493f4383732fe408118c75ec8093d49b19f9492d
M0e <mghas2@uic.edu>
2018-01-14 05:48:50 -0600

    add replayMode feature :sparkles:

--- js/attentionCenter.js
+++ js/attentionCenter.js
@@ -2,11 +2,15 @@
 
 window.addEventListener('load', () => {
     //get setting info from local storage when page loads
-    chrome.storage.local.get(['isRunning', 'removeSug', 'theaterMode', 'removeComments'], value => {
+    //chrome.storage.local.get(['isRunning', 'removeSug', 'theaterMode', 'removeComments', 'replayMode', 'stopAutoPlayMode'], value => {
+    chrome.storage.local.get(['isRunning', 'removeSug', 'theaterMode', 'removeComments', 'replayMode'], value => {
         document.getElementById('control-button').innerText = (value["isRunning"] ? "Pause" : "Resume");
         value['removeSug'] != null ? document.getElementById('remove-suggestions').checked = value["removeSug"] : null;
         value['theaterMode'] != null ? document.getElementById('theatermode').checked = value["theaterMode"] : null;
         value['removeComments'] != null ? document.getElementById('remove-comments').checked = value["removeComments"] : null;
+        value['replayMode'] != null ? document.getElementById('replaymode').checked = value["replayMode"] : null;
+        //value['stopAutoPlayMode'] != null ? document.getElementById('stopautoplaymode').checked = value["stopAutoPlayMode"] : null;
+
     });
     //get element info of loaded page
     document.getElementById('control-button').addEventListener('click', () => {chrome.storage.local.get(["isRunning"], value => {controlCommand("control", value["isRunning"]);}) });
@@ -20,7 +24,9 @@ var saveSettings = () => {
     chrome.storage.local.set({
         "removeSug": document.getElementById('remove-suggestions').checked,
         "theaterMode": document.getElementById('theatermode').checked,
-        "removeComments": document.getElementById('remove-comments').checked
+        "removeComments": document.getElementById('remove-comments').checked,
+        "replayMode": document.getElementById('replaymode').checked
+        //"stopAutoPlayeMode": document.getElementById('stopautoplaymode').checked
     });
     controlCommand("save_settings");
 };
--- js/background.js
+++ js/background.js
@@ -1,11 +1,12 @@
 "use strict";
 
 //initialization
-chrome.storage.local.set({'isRunning': true, 'removeSug': true, 'theaterMode': true, 'removeComments': true});
+chrome.storage.local.set({'isRunning': true, 'removeSug': true, 'theaterMode': true, 'removeComments': true, 'replayMode': false});
 var isRunning = true;
 var removeSug = true;
 var theaterMode = true;
 var removeCom = true;
+var replayMode = false;
 
 //inject code
 const removeSuggestions = ` 
@@ -15,6 +16,9 @@ const removeComments = `
     var comments = document.querySelector("ytd-comments");
     comments != null ? comments.remove() : null;`;
 //We set wide = 1 or 0 in cookies for theater mode
+
+const replay = `document.getElementsByClassName('ytp-play-button ytp-button')[0].click();`;
+
 
 //popup
 chrome.browserAction.onClicked.addListener(() => { chrome.tabs.create({url: chrome.extension.getURL('attentionCenter.html'), "active": true}) });
@@ -40,11 +44,26 @@ chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
 //injection
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete'){ 
-        var injectedJSCode = 'document.addEventListener("DOMSubtreeModified", () => {'
+        var injectedJSCode = 'document.addEventListener("DOMSubtreeModified", () => {';
         if (isSubstring('.youtube.', tab.url) &&  isRunning) {
+            var observerJSCode = `
+            if (observer) { observer.disconnect(); }
+            var button_node = document.getElementsByClassName('ytp-play-button ytp-button')[0];
+            var config = { attributes: true };
+            var observer = new MutationObserver((mutationsList) => {
+                for(var mutation of mutationsList) {
+                    if (mutation.type == 'attributes') {
+                        if (button_node.getAttribute(mutation.attributeName) == 'Replay'){`
+                        + (replayMode ? replay : 'console.log("disabled");') +
+                       `}
+                    }
+                }
+            });`;
             injectedJSCode += (removeSug ? removeSuggestions : '');
             injectedJSCode += (removeCom ? removeComments : '');
+            injectedJSCode += (replayMode ? observerJSCode + 'observer.observe(button_node, config);' : 'if (observer) { observer.disconnect(); }');
             injectedJSCode += '});'
+            console.log(injectedJSCode);
             chrome.tabs.executeScript(tabId, {code: injectedJSCode});
             console.log(tab.url);
             //set wide = 1 for theater mode, wide = 0 for non-theater mode
@@ -55,12 +74,13 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 });
 //update settings
 var refreshSettings = () => {
-    chrome.storage.local.get(['isRunning', 'removeSug', 'theaterMode', 'removeComments'], value => 
+    chrome.storage.local.get(['isRunning', 'removeSug', 'theaterMode', 'removeComments', 'replayMode'], value => 
     {
       isRunning = value["isRunning"];
       removeSug = value["removeSug"];
       theaterMode = value["theaterMode"];
       removeCom = value["removeComments"];
+      replayMode = value["replayMode"];
     });
   };
 //check string
